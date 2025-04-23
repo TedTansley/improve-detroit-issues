8f44d12f0379bce47fedec2d94bfdff5e8fc52f0
TedTansley <103963155+TedTansley@users.noreply.github.com>
2025-04-23 14:28:39 -0400

    Update gis_to_bigquery.py

--- gis_to_bigquery.py
+++ gis_to_bigquery.py
@@ -146,7 +146,7 @@ def save_to_csv(all_data):
 
     # Save DataFrame to CSV
     local_csv_path = "/tmp/gis_data.csv"  # Temporary path for CSV file
-    df.to_csv(local_csv_path, index=False, quoting=csv.QUOTE_ALL)
+    df.to_csv(local_csv_path, index=False, quoting=csv.QUOTE_ALL, na_rep='')
 
     print(f"Data saved as CSV at {local_csv_path}")
     update_bigquery_from_csv(local_csv_path)
